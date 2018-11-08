7e7a8efd547defd8e64be13b775f7f819da0c11f
vijaypandiyan <vijay.pandiyan@gmail.com>
2018-11-08 07:56:05 -0800

    initial react setup

---
+++ src/App.js
@@ -0,0 +1,4 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {Navbar} from "./components/navbar"
+ReactDOM.render(<div><Navbar/>,</div>,document.getElementById('react-container'));
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/entry.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  } ,
+  module:{
+      rules:[{
+          test:/\.js$/,
+          exclude :/node_modules/,
+          use:{
+              loader:'babel-loader'
+          }
+      },
+      {
+        test:/\.html$/,       
+        use:{
+            loader:'html-loader'
+        }
+    }]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),   
+    port: 9000
+  }
+  
+};
